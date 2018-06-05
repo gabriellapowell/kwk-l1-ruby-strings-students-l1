f5df97b20495085589d9d7c5d0e9150ee0a4562d
gabriellapowell <soccerplayer60@icloud.com>
2018-06-05 17:08:50 +0000

    automatically backed up by learn

---
+++ greeting.rb
@@ -0,0 +1,4 @@
+def say_hello(your_name,my_name = king kong)
+puts "hello #{your_name} I'm #{my_name}!"
+end
+say_hello ("Hibba")
--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,24 @@
-# Code your prompts here!
+# Code your prompts her
+puts "Hi you've been invited to a party!What is your name?"
+guest_name = gets.chomp
+
+puts "What is the name of the party?"
+party_name = gets.chomp
+
+puts "When will the party be?"
+date = gets.chomp 
+
+puts "What time will the party be at?"
+time = gets.chomp 
+
+puts "What is your name?"
+host_name = gets.chomp
 
 # Try starting out with puts'ing a string.
+puts "Dear #{guest_name},
+
+You are cordially invited to #{party_name} on #{date} at #{time}. I hope to see you there!
+
+Sincerely,
+
+#{host_name}"
